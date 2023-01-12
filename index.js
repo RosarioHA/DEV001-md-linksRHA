1991e099b6950547e8ee1ffd539f2c0f5cbb6b0b
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-01-12 18:26:00 -0300

    instalar dependencias, primeros pasos

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+
+const mdLinks = (path, options) => {
+  return new Promise ((resolve, reject) => {
+    // identificar si existe una ruta.
+    if (fs.existsSync(path)){
+    // chequear si es una ruta absoluta. Si no, convertirla en una.
+    } else {
+      // si no existe una ruta, se rechaza la promesa.
+      reject('ésta ruta no existe');
+    };
+  });
+};
+
+module.exports = {
+  mdLinks,
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,12 @@
-const mdLinks = require('../');
-
+const { mdLinks } = require('../index.js');
 
 describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
-
+  // it('debería devolver una promesa', () => {
+  // expect(mdLinks()).toBe(typeof Promise);
+  // });
+  it('debe rechazar la promesa si no encuentra un path', () => {
+    return mdLinks('fake/path.md').catch((error) => {
+expect(error).toBe('ésta ruta no existe');
+    });
+    });
 });
