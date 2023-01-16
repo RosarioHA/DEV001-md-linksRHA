6f48b02b68cd5486c92d9e366094a8b471e61a96
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-01-16 18:56:09 -0300

    aislar funcion pathExists

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const fs = require('fs');
 
+// función que analiza si existe el path
+const pathExists = (path) => {
+  fs.existsSync(path);
+};
+
 const mdLinks = (path, options) => new Promise((resolve, reject) => {
   // identificar si existe una ruta.
-  if (fs.existsSync(path)) {
+  if (pathExists(path)) { // si no, lleva al mensaje de error, si sí, entra al if
     resolve('la ruta existe');
+    // función toAbsolute que analiza si es absoluta o relativa, convierte a absoluta
+    // función fileOrDoc revisa si es archivo o directorio. Si es archivo entra al if, si no va a error
+    // función fileType ve si es un archivo .md. Si sí entra al if y si no va a error.
+    // función pathsArray que retorna un array de archivos .md
+      //if (){ //el array tiene contenido (pathsArray.lenght), entra al if. Si no va a error.
+        // recorrer los archivos con pathsArray.forEach y ver si son válidos
+
+      }
   } else {
     // si no existe una ruta, se rechaza la promesa.
     reject('ésta ruta no existe');
   }
-});
+);
 
 module.exports = {
   mdLinks,
 };
 
-// chequear si es una ruta absoluta. Si no, CONVERTIRLA EN UNA.
-// chequear si es un archivo o directorio.
-// chequear si es un archivo .md: ver funcion de node para verificar extención y compararla con .md
-// chequear si el archivo contiene links
-// OBTENER LINKS
-// son links válidos?
-// GENERAR INFORME
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,4 @@
-const { mdLinks } = require('../index.js');
+const { mdLinks } = require('../index');
 
 describe('mdLinks', () => {
   // it('debería devolver una promesa', () => {
