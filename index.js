1315617d63834b1bf0e21b459f344ee6eafede07
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-01-16 17:36:59 -0300

    arreglar problemas de eslint

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const fs = require('fs');
 
 const mdLinks = (path, options) => {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     // identificar si existe una ruta.
-    if (fs.existsSync(path)){
+    if (fs.existsSync(path)) {
       resolve();
     // chequear si es una ruta absoluta. Si no, CONVERTIRLA EN UNA.
       // chequear si es un archivo o directorio.
-        // chequear si es un archivo .md: ver funcion de node para verificar extención y compararla con .md
-          //chequear si el archivo contiene links
-          // OBTENER LINKS
-            // son links válidos?
+      // chequear si es un archivo .md: ver funcion de node para verificar extención y compararla con .md
+      // chequear si el archivo contiene links
+      // OBTENER LINKS
+      // son links válidos?
       // GENERAR INFORME
     } else {
       // si no existe una ruta, se rechaza la promesa.
       reject('ésta ruta no existe');
-    };
+    }
   });
 };
 
